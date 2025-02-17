b58e6be6fc5b98dda36d32d331722db62e7b4293
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2025-02-17 15:23:39 -0800

    Update Action (#4)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Release](https://img.shields.io/github/actions/workflow/status/smashedr/docker-tags-action/release.yaml?logo=github&logoColor=white&label=tags)](https://github.com/smashedr/docker-tags-action/actions/workflows/release.yaml)
+[![Release](https://img.shields.io/github/actions/workflow/status/smashedr/docker-tags-action/release.yaml?logo=github&logoColor=white&label=release)](https://github.com/smashedr/docker-tags-action/actions/workflows/release.yaml)
 [![Test](https://img.shields.io/github/actions/workflow/status/smashedr/docker-tags-action/test.yaml?logo=github&logoColor=white&label=test)](https://github.com/smashedr/docker-tags-action/actions/workflows/test.yaml)
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=smashedr_docker-tags-action&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=smashedr_docker-tags-action)
 [![GitHub Release Version](https://img.shields.io/github/v/release/smashedr/docker-tags-action?logo=github)](https://github.com/smashedr/docker-tags-action/releases/latest)
@@ -10,45 +10,118 @@
 
 # Docker Tags Action
 
-Generate Docker Tags. For a more detailed implementation see: https://github.com/docker/metadata-action
-
-> [!NOTE]  
-> Please submit
-> a [Feature Request](https://github.com/smashedr/docker-tags-action/discussions/categories/feature-requests)
-> for new features or [Open an Issue](https://github.com/smashedr/docker-tags-action/issues) if you find any bugs.
-
 - [Inputs](#Inputs)
 - [Outputs](#Outputs)
 - [Examples](#Examples)
 - [Support](#Support)
 - [Contributing](#Contributing)
 
+Generate Docker Tags. For a more detailed implementation see: https://github.com/docker/metadata-action
+
+> [!NOTE]  
+> Please submit a [Feature Request](https://github.com/smashedr/docker-tags-action/discussions/categories/feature-requests)
+> for new features or [Open an Issue](https://github.com/smashedr/docker-tags-action/issues) if you find any bugs.
+
 ## Inputs
 
-| input     | required | default   | description                           |
-| --------- | -------- | --------- | ------------------------------------- |
-| images    | Yes      | -         | Image to Generate Tag for             |
-| extra     | No       | -         | Extra Tags to Generate \*             |
-| seperator | No       | `\n`      | Tag Seperator for Output              |
-| latest    | No       | `default` | Latest tag: [true, false, default] \* |
+| input     | required | default                            | description                               |
+| --------- | -------- | ---------------------------------- | ----------------------------------------- |
+| images    | No       | `ghcr.io/${{ github.repository }}` | Images for Tag Generation, CSV or Newline |
+| tags      | No       | _[see tags](#tags)_                | Extra Tags to Generate, CSV or Newline    |
+| labels    | No       | _[see labels](#labels)_            | Extra Labels to Generate, CSV or Newline  |
+| seperator | No       | `\n`                               | Output Seperator                          |
+| latest    | No       | `default`                          | Latest Tag: [true, false, default] \*     |
+| summary   | No       | `true`                             | Add Summary to Job                        |
 
-**extra** - Extra tags can be newline or comma seperated.
-
-**latest** - Default behavior only adds `latest` tag to a release that are not a pre-release.
+This works with no inputs, but you can customize the resulting tags and labels with inputs.
 
 ```yaml
 - name: 'Docker Tags'
   id: tags
   uses: smashedr/docker-tags-action@master
-  with:
-    images: 'ghcr.io/${{ github.repository }}'
+```
+
+**seperator** - Output seperator. Newline is default and that works as the input for the docker build actions.
+
+**latest** - Default behavior only adds `latest` tag to a release that are not a pre-release.
+
+**summary** - Write a Summary for the job. To disable this set to `false`.
+
+<details><summary>📜 View Example Summary</summary>
+
+---
+
+### Docker Tags Action
+
+Generated **2** Tag(s) and **8** Label(s) for **1** Image(s).
+
+Docker Tags 2
+
+<pre lang="plain"><code>ghcr.io/smashedr/docker-tags-action:pr-4
+ghcr.io/smashedr/docker-tags-action:latest</code></pre>
+
+Docker Labels 8
+
+<pre lang="plain"><code>org.opencontainers.image.created=2025-02-17T23:03:18.554Z
+org.opencontainers.image.revision=b812bb5c006ecade3ff39b386c9174732bce9a30
+org.opencontainers.image.source=https://github.com/smashedr/docker-tags-action
+org.opencontainers.image.title=docker-tags-action
+org.opencontainers.image.url=https://github.com/smashedr/docker-tags-action
+org.opencontainers.image.version=pr-4
+org.opencontainers.image.description=Docker Tags Action
+org.opencontainers.image.authors=smashedr</code></pre>
+<details><summary>Inputs</summary><table><tr><th>Input</th><th>Value</th></tr><tr><td>images</td><td>ghcr.io/smashedr/docker-tags-action</td></tr><tr><td>tags</td><td>-</td></tr><tr><td>labels</td><td>org.opencontainers.image.licenses=,org.opencontainers.image.authors=smashedr</td></tr><tr><td>seperator</td><td>"\n"</td></tr><tr><td>latest</td><td>true</td></tr><tr><td>summary</td><td>true</td></tr></table></details>
+
+[View Documentation](https://github.com/smashedr/docker-tags-action/tree/refs/heads/updates?tab=readme-ov-file#readme) | [Report an Issue or Request a Feature](https://github.com/smashedr/docker-tags-action/issues)
+
+---
+
+</details>
+
+### tags
+
+| Event             | Ref                 | Tags     |
+| ----------------- | ------------------- | -------- |
+| `tag` / `release` | `refs/tags/v1.0.0`  | `v1.0.0` |
+| `push` / `other`  | `refs/heads/master` | `master` |
+| `pull_request`    | `refs/pull/1/merge` | `pr-1`   |
+
+This is the default tag added which is also set as the version for the label.
+
+Coming Soon: Add an option called `default` to disable or override the default tag.
+
+### labels
+
+```shell
+org.opencontainers.image.description=Example Repository Description
+org.opencontainers.image.revision=32b96cee5b4e940b4023f78261702470d59c8001
+org.opencontainers.image.source=https://github.com/smashedr/docker-tags-action
+org.opencontainers.image.title=repository-name
+org.opencontainers.image.url=https://github.com/smashedr/docker-tags-action
+org.opencontainers.image.version=v1.0.0
+org.opencontainers.image.licenses=GPL-3.0
+```
+
+These are the default labels. You can add them or remove them by providing a key with no value.
+
+The description and license are only added if they are defined on your repository.
+
+Example removing `org.opencontainers.image.licenses` and adding `org.opencontainers.image.authors`.
+
+```yaml
+labels: |
+  org.opencontainers.image.licenses=
+  org.opencontainers.image.authors=smashedr
 ```
 
 ### Outputs
 
-| output | description              |
-| ------ | ------------------------ |
-| tags   | Tags Generated for Image |
+| output | description      |
+| ------ | ---------------- |
+| tags   | Generated Tags   |
+| labels | Generated Labels |
+
+All outputs are seperated by the inputs `seperator` which defaults to a newline.
 
 ```yaml
 - name: 'Docker Tags'
@@ -58,12 +131,83 @@ Generate Docker Tags. For a more detailed implementation see: https://github.com
     images: 'ghcr.io/${{ github.repository }}'
 
 - name: 'Echo Result'
-  run: echo '${{ steps.tags.outputs.tags }}'
+  run: |
+    echo tags: '${{ steps.tags.outputs.tags }}'
+    echo labels: '${{ steps.tags.outputs.labels }}'
 ```
 
 ## Examples
 
-Coming Soon...
+With all inputs:
+
+```yaml
+- name: 'Docker Tags'
+  id: tags
+  uses: smashedr/docker-tags-action@master
+  with:
+    images: 'ghcr.io/${{ github.repository }}'
+    tags: v1,v1.0
+    labels: |
+      org.opencontainers.image.licenses=
+      org.opencontainers.image.authors=smashedr
+    seperator: ','
+    latest: true
+```
+
+Full Example:
+
+```yaml
+name: 'Release'
+
+on:
+  release:
+    types: [published]
+
+jobs:
+  build:
+    name: 'Build'
+    runs-on: ubuntu-latest
+    timeout-minutes: 5
+    permissions:
+      packages: write
+
+    steps:
+      - name: 'Checkout'
+        uses: actions/checkout@v4
+
+      - name: 'Docker Tags'
+        id: tags
+        uses: smashedr/docker-tags-action@master
+        with:
+          images: 'ghcr.io/${{ github.repository }}'
+          extra: ${{ github.ref_name }}
+          latest: false
+
+      - name: 'Echo Tags'
+        run: |
+          echo -e "tags: \n${{ steps.tags.outputs.tags }}"
+          echo -e "labels: \n${{ steps.tags.outputs.labels }}"
+
+      - name: 'Docker Login'
+        uses: docker/login-action@v3
+        with:
+          registry: 'ghcr.io'
+          username: ${{ vars.GHCR_USER }}
+          password: ${{ secrets.GHCR_PASS }}
+
+      - name: 'Setup Buildx'
+        uses: docker/setup-buildx-action@v3
+
+      - name: 'Build and Push'
+        uses: docker/build-push-action@v6
+        with:
+          context: .
+          push: true
+          tags: ${{ steps.tags.outputs.tags }}
+          labels: ${{ steps.tags.outputs.labels }}
+          cache-from: type=gha
+          cache-to: type=gha,mode=max
+```
 
 To see this used in a release workflow, see: https://github.com/cssnr/cloudflare-purge-cache-action/blob/master/.github/workflows/release.yaml
 
--- dist/index.js
+++ dist/index.js
@@ -33612,52 +33612,60 @@ exports.parse = parse;
 var __webpack_exports__ = {};
 const core = __nccwpck_require__(7484)
 const github = __nccwpck_require__(3228)
-// const semver = require('semver')
 const { parse } = __nccwpck_require__(1110)
 
 ;(async () => {
     try {
+        core.info(`🏳️ Starting Docker Tags Action`)
+
+        // Debug
+        // console.log('process.env:', process.env)
         // console.log('github.context:', github.context)
 
-        // Parse Ref
         console.log('github.context.ref:', github.context.ref)
         console.log('github.context.eventName:', github.context.eventName)
         console.log('prerelease:', github.context.payload.release?.prerelease)
 
-        const value = github.context.ref.split('/', 2).join('/') + '/'
-        console.log('value:', value)
-        const ref = github.context.ref.replace(value, '')
-        console.log('ref:', ref)
+        // Parse ref
+        let ref = github.context.ref.split('/')[2]
+        if (github.context.ref.startsWith('refs/pull/')) {
+            console.log('Pull Request Detected:', ref)
+            ref = `pr-${ref}`
+        }
+        if (!ref) {
+            return core.setFailed(`Unable to parse ref: ${github.context.ref}`)
+        }
+        core.info(`ref: \u001b[32;1m${ref}`)
 
         // Process Inputs
-        const images = core.getInput('images')
+        // core.info(`images: "${images}"`)
+        const images = parse(core.getInput('images', { required: true }), {
+            delimiter: ',',
+            trim: true,
+            relax_column_count: true,
+        }).flat()
         console.log('images:', images)
-        const extra = core.getInput('extra')
-        console.log('extra:', extra)
+        const tags = core.getInput('tags')
+        console.log('tags:', tags)
+        const labels = core.getInput('labels')
+        console.log('labels:', labels)
         const seperator =
             core.getInput('seperator', { trimWhitespace: false }) || `\n`
         console.log('seperator:', JSON.stringify(seperator))
         const latest = core.getInput('latest')
         console.log('latest:', latest)
-        // const prefix = core.getInput('prefix')
-        // console.log('prefix:', prefix)
+        const summary = core.getBooleanInput('summary')
+        console.log('summary:', summary)
 
-        // // Set Variables
-        // const { owner, repo } = github.context.repo
-        // console.log('owner:', owner)
-        // console.log('repo:', repo)
-        // const version = semver.parse(ref)
-        // console.log('version:', version)
+        // Set Variables
+        const repo = github.context.payload.repository
+        console.log('name:', repo.name)
+        console.log('description:', repo.description)
+        console.log('html_url:', repo.html_url)
+        console.log('spdx_id:', repo.license?.spdx_id)
 
-        // Parse Images
-        const parsedImages = parse(images, {
-            delimiter: ',',
-            trim: true,
-            relax_column_count: true,
-        }).flat()
-        console.log('parsedImages:', parsedImages)
-
-        // Collect Tags
+        // Process Tags
+        core.info('⌛ Processing Tags')
         const collectedTags = []
         if (ref) {
             collectedTags.push(ref)
@@ -33667,15 +33675,15 @@ const { parse } = __nccwpck_require__(1110)
                 github.context.eventName === 'release' &&
                 !github.context.payload.release?.prerelease
             ) {
-                console.log('++++ LATEST +++++')
+                console.log('\u001b[33;1mAdding latest tag on: release')
                 collectedTags.push('latest')
             }
         } else if (latest === 'true') {
+            console.log('\u001b[33;1mAdding latest tag on: true')
             collectedTags.push('latest')
         }
-
-        if (extra) {
-            const parsedTags = parse(extra, {
+        if (tags) {
+            const parsedTags = parse(tags, {
                 delimiter: ',',
                 trim: true,
                 relax_column_count: true,
@@ -33684,27 +33692,127 @@ const { parse } = __nccwpck_require__(1110)
             collectedTags.push(...parsedTags)
         }
         console.log('collectedTags:', collectedTags)
-
-        const tags = [...new Set(collectedTags)]
-        console.log('tags:', tags)
-
-        // Process Results
+        const allTags = [...new Set(collectedTags)]
+        console.log('allTags:', allTags)
         const dockerTags = []
-        for (const image of parsedImages) {
-            for (const tag of tags) {
+        for (const image of images) {
+            for (const tag of allTags) {
                 dockerTags.push(`${image}:${tag}`)
             }
         }
         console.log('dockerTags:', dockerTags)
-        const output = dockerTags.join(seperator)
-        console.log('output:', output)
-        core.setOutput('tags', output)
+
+        // Process Labels
+        core.info('⌛ Processing Labels')
+        const defaultLabels = {
+            'org.opencontainers.image.created': new Date().toISOString(),
+            'org.opencontainers.image.revision': github.context.sha,
+            'org.opencontainers.image.source': repo.html_url,
+            'org.opencontainers.image.title': repo.name,
+            'org.opencontainers.image.url': repo.html_url,
+            'org.opencontainers.image.version': ref,
+        }
+        if (repo.description) {
+            defaultLabels['org.opencontainers.image.description'] =
+                repo.description
+        }
+        if (repo.license?.spdx_id) {
+            defaultLabels['org.opencontainers.image.licenses'] =
+                repo.license.spdx_id
+        }
+        // console.log('defaultLabels:', defaultLabels)
+        if (labels) {
+            const parsedLabels = parse(labels, {
+                delimiter: ',',
+                trim: true,
+                relax_column_count: true,
+            }).flat()
+            console.log('parsedLabels:', parsedLabels)
+            for (const label of parsedLabels) {
+                if (!label.includes('=')) {
+                    return core.setFailed(
+                        `Label provided without an = symbol: ${label}`
+                    )
+                }
+                const [key, value] = label.split(/=(.*)/s).slice(0, 2)
+                if (value) {
+                    console.log(`\u001b[32;1mAdding: \u001b[0m${key}=${value}`)
+                    defaultLabels[key] = value
+                } else {
+                    console.log(`\u001b[31;1mDeleting: \u001b[0m${key}`)
+                    delete defaultLabels[key]
+                }
+            }
+        }
+        // console.log('defaultLabels:', defaultLabels)
+        const dockerLabels = []
+        for (const [key, value] of Object.entries(defaultLabels)) {
+            dockerLabels.push(`${key}=${value}`)
+        }
+        console.log('dockerLabels:', dockerLabels)
+
+        // Set Outputs
+        core.info('📩 Setting Outputs')
+        core.setOutput('tags', dockerTags.join(seperator))
+        core.setOutput('labels', dockerLabels.join(seperator))
+
+        // Summary
+        if (summary) {
+            core.info('📝 Writing Job Summary')
+            const inputs_table = gen_inputs_table({
+                images: images.join(','),
+                tags: tags.replaceAll('\n', ','),
+                labels: labels.replaceAll('\n', ','),
+                seperator: JSON.stringify(seperator),
+                latest: latest,
+                summary: summary,
+            })
+            core.summary.addRaw('### Docker Tags Action', true)
+            core.summary.addRaw(
+                `Generated **${dockerTags.length / images.length}** Tag(s) and **${dockerLabels.length / images.length}** Label(s) for **${images.length}** Image(s).\n`,
+                true
+            )
+            core.summary.addRaw(`Docker Tags ${dockerTags.length}`, true)
+            core.summary.addCodeBlock(dockerTags.join('\n'), 'plain')
+            core.summary.addRaw(`Docker Labels ${dockerLabels.length}`, true)
+            core.summary.addCodeBlock(dockerLabels.join('\n'), 'plain')
+            core.summary.addRaw(inputs_table, true)
+            core.summary.addRaw(
+                '\n[View Documentation](https://github.com/smashedr/docker-tags-action/tree/refs/heads/updates?tab=readme-ov-file#readme) | '
+            )
+            core.summary.addRaw(
+                '[Report an Issue or Request a Feature](https://github.com/smashedr/docker-tags-action/issues)',
+                true
+            )
+            await core.summary.write()
+        } else {
+            core.info('⏩ Skipping Job Summary')
+        }
+
+        core.info(`✅ \u001b[32;1mFinished Success`)
     } catch (e) {
         core.debug(e)
         core.info(e.message)
         core.setFailed(e.message)
     }
 })()
+
+/**
+ * @function gen_inputs_table
+ * @param {Object} inputs
+ * @return String
+ */
+function gen_inputs_table(inputs) {
+    const table = [
+        '<details><summary>Inputs</summary>',
+        '<table><tr><th>Input</th><th>Value</th></tr>',
+    ]
+    for (const [key, object] of Object.entries(inputs)) {
+        const value = object.toString() || '-'
+        table.push(`<tr><td>${key}</td><td>${value}</td></tr>`)
+    }
+    return table.join('') + '</table></details>'
+}
 
 module.exports = __webpack_exports__;
 /******/ })()
